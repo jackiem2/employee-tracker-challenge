1d57ed74e89893bcf7dda0d7aca1b417ccbbda21
jackiem2 <jacqueline.merlan@gmail.com>
2022-03-11 14:02:01 -0600

    added info for department, roles, employee

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO department (name)
+VALUES ("engineering"),
+    ("finance"),
+    ("sales"),
+    ("legal");
+
+    SELECT * FROM DEPARTMENT;
+
+INSERT INTO roles (tilte, salary, department_id)
+VALUES ("software engineer", 120000, 1),
+    ("lead engineer", 150000, 1),
+    ("account manager", 160000, 2),
+    ("accountant", 125000, 2),
+    ("salesperson", 80000, 3),
+    ("legal team lead", 250000, 4),
+    ("lawyer", 190000, 4);
+
+    SELECT * FROM roles
+
+INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+VALUES ("Kevin", "Tupik", 1, 1 ),
+    ("Ashley", "Rodriguez", 1, NULL),
+    ("Kumal", "Sing", 2, NULL),
+    ("Malia", "Brown", 2, 2),
+    ("Mike", "Chan", 3, 2),
+    ("Sarah", "Lourd", 4, NULL),
+    ("Tom", "Allen", 4, 4);
+
+    SELECT * FROM employee
+
+
